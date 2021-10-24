c3275edee81bcb0db0364bfb27f209cc16cf1574
IvanCastillo2002 <90781328+IvanCastillo2002@users.noreply.github.com>
2021-10-23 23:03:25 -0600

    Add files via upload

---
+++ EVA_1_1_TIPOS_DE_DATOS_PRIMITIVOS/src/Main.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ivan Eduardo Castillo Castro
+ * Fecha de Creacion: 30 de Septiembre del 2021
+ */
+public class Main {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+      int iVal=Integer.MAX_VALUE;
+      System.out.println("Valor mas Grande: " + iVal);
+      iVal +=1;
+      System.out.println("Valor mas Grande: " + iVal);
+      iVal=Integer.MIN_VALUE;
+      System.out.println("Valor mas Pequeño: " + iVal);
+      
+      
+     for(int i=0; i<10; i++){
+        String mensaje = "Hola mundo";
+        System.out.println("mensaje" + i);
+     }
+     
+       
+
+      
+      
+              
+   }
+   
+}
